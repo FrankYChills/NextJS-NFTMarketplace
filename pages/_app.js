7a63b22201f9a876da1ae90e68b64a1027b2f97e
franky <itskchills11@gmail.com>
2022-11-29 14:42:58 +0530

    basic structure defined

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { MoralisProvider } from "react-moralis";
+import Header from "../components/Header";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>NFT Marketplace</title>
+        <meta name="nft-marketplace" content="Sell and Buy Nfts" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <MoralisProvider initializeOnMount={false}>
+        <Header />
+        <Component {...pageProps} />
+      </MoralisProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/sell-nft.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function sellnft() {
+  return <div>This is the page for registering your nft to sell</div>;
+}
+
+export default sellnft;
